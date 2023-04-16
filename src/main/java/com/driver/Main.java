b74ca7bdcae947b7f47e4e2e972ed20a46a36246
Utkarsh Shukla <63734211+Utkarsh-Technical@users.noreply.github.com>
2023-04-16 21:41:06 +0530

    Assignment done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,26 @@
 package com.driver;
-
+import java.util.*;
 public class Main {
+    public static class Product{
+        public int product(int x, int y) {
+            return x*y;
+        }
 
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+
+        public double product(double x, double y) {
+            return x*y;
+        }
+    }
+
+    public static void main(String []args) {
+
+        Product p = new Product();
+        int p1 = p.product(10,20);
+        int p2 = p.product(10,20,30);
+        double p3 = p.product(10.6,20.5);
+        System.out.println(p1+ " "+p2 +" "+p3);
+    }
 }
